3bc00af00ed0d51f7a8c3ae478633bcb5bba195b
Vechtomova Anastasiia <vecht0mova@yandex.ru>
2025-09-09 22:16:53 +0300

    Node. Написала код не сама, понимание немного есть, но очень сложно.

---
+++ src/level_14/lecture_5/Solution_3_1/Main.java
@@ -0,0 +1,18 @@
+package level_14.lecture_5.Solution_3_1;
+
+public class Main extends StringLinkedList{
+
+    public static void main(String[] args) {
+        StringLinkedList stringLinkedList = new StringLinkedList();
+        stringLinkedList.add("1");
+        stringLinkedList.add("2");
+        stringLinkedList.add("3");
+        stringLinkedList.add("4");
+        stringLinkedList.add("5");
+        stringLinkedList.add("6");
+        stringLinkedList.add("7");
+        stringLinkedList.add("8");
+        stringLinkedList.add("9");
+        stringLinkedList.printAll();
+    }
+}
