d0b2fd094978c64be7eda1b69704b6ad18d51a14
Vechtomova Anastasiia <vecht0mova@yandex.ru>
2025-09-12 14:13:29 +0300

    commit 14.5.1 переделала через enum, получилось только через sout

--- src/level_14/lecture_7/Solution_4_1.java
+++ src/level_14/lecture_7/Solution_4_1.java
@@ -1,5 +1,7 @@
 package level_14.lecture_7;
 
+import java.util.Arrays;
+
 /**
  * В классе Solution есть метод getMonthByIndex(int), который возвращает название месяца по его номеру,
  * начиная с 1. Твоя задача — переписать метод, используя только оператор switch и ключевые слова case, break и default.
@@ -11,53 +13,28 @@ package level_14.lecture_7;
 public class Solution_4_1 {
 
     public static void main(String[] args) {
-        System.out.println(getMonthByIndex(4));
-        System.out.println(getMonthByIndex(8));
-        System.out.println(getMonthByIndex(12));
+        getMonthByIndex();
     }
 
-    public static String getMonthByIndex(int monthIndex) {
-        String monthString = "-";
-        switch (monthIndex) {
-            case 1:
-                System.out.println("Январь");
-                break;
-            case 2:
-                System.out.println("Февраль");
-                break;
-            case 3:
-                System.out.println("Март");
-                break;
-            case 4:
-                System.out.println("Апрель");
-                break;
-            case 5:
-                System.out.println("Май");
-                break;
-            case 6:
-                System.out.println("Июнь");
-                break;
-            case 7:
-                System.out.println("Июль");
-                break;
-            case 8:
-                System.out.println("Август");
-                break;
-            case 9:
-                System.out.println("Сентябрь");
-                break;
-            case 10:
-                System.out.println("Октябрь");
-                break;
-            case 11:
-                System.out.println("Ноябрь");
-                break;
-            case 12:
-                System.out.println("Декабрь");
-                break;
-            default:
-                System.out.println("Такого месяца не существует");
-        }
-        return monthString;
+    enum MonthOfYear {
+        JUNARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER;
+    }
+
+    public static void getMonthByIndex() {
+        MonthOfYear[] allMonths = new MonthOfYear[] {MonthOfYear.values()[2]};
+        MonthOfYear[] all = new MonthOfYear[] {MonthOfYear.values()[7]};
+        System.out.println(Arrays.toString(allMonths));
+        System.out.println(Arrays.toString(all));
     }
 }
